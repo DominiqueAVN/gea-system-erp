fede5fa4a2227e7b70b8666581b87b101f499114
Dominique Avendaño <dominiqueavendanowca@gmail.com>
2026-07-30 01:16:15 -0500

    Update robotroots.py

--- robotroots.py
+++ robotroots.py
@@ -1,9 +1,3 @@
-import subprocess
-import sys
-try:
-    import openpyxl
-except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "openpyxl"])
 import streamlit as st
 import sqlite3
 import pandas as pd
